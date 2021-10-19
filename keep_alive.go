9d18cc2e2c4ea4f882c593407308f44908e475bc
kartik141 <75618573+kartik141@users.noreply.github.com>
2021-10-19 18:31:56 +0530

    Update keep_alive.go

--- keep_alive.go
+++ keep_alive.go
@@ -9,8 +9,5 @@ func handler(w http.ResponseWriter, r *http.Request){
   io.WriteString(w, "Hello World")
 }
 
-func main() {
-  http.HandleFunc("/", handler)
-  http.ListenAndServe(":8000", nil)
-}
+
 
